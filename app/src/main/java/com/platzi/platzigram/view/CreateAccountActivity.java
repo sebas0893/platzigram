113bc5a5f33b2bedab90f08473846fcbd689dca7
Sebas García <sebas0893@gmail.com>
2018-03-31 18:53:51 -0500

    Se crea activity para crear cuenta

--- app/src/main/java/com/platzi/platzigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/platzi/platzigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.platzi.platzigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.platzi.platzigram.R;
 
@@ -11,5 +12,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getResources().getString(R.string.toolbar_title_createaccount), false);
+    }
+
+    public void showToolBar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
