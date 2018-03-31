d8c1b1391c3d928dfc820635d78776fb94d0444f
Sebas García <sebas0893@gmail.com>
2018-03-30 23:28:58 -0500

    Se crea el activity Login

---
+++ app/src/main/java/com/platzi/platzigram/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.platzi.platzigram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+
+    }
+}
