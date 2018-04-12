a4d43f9eec9be6ce605dee518b8ef9f33c205930
Sebas García <sebas0893@gmail.com>
2018-04-11 23:09:56 -0500

    Se implementa el RecycerView

---
+++ app/src/main/java/com/platzi/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,68 @@
+package com.platzi.platzigram.adapter;
+
+import android.app.Activity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.platzi.platzigram.R;
+import com.platzi.platzigram.model.Picture;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sebas on 11/04/2018.
+ */
+
+public class PictureAdapterRecyclerView extends RecyclerView.Adapter<PictureAdapterRecyclerView.PictureViewHolder> {
+
+    private ArrayList<Picture> pictures;
+    private int resource;
+    private Activity activity;
+
+    public PictureAdapterRecyclerView(ArrayList<Picture> pictures, int resource, Activity activity) {
+        this.pictures = pictures;
+        this.resource = resource;
+        this.activity = activity;
+    }
+
+    @Override
+    public PictureViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource, parent, false);
+        return new PictureViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PictureViewHolder holder, int position) {
+        Picture picture = pictures.get(position);
+        holder.usernameCard.setText(picture.getUserName());
+        holder.timeCard.setText(picture.getTime());
+        holder.likeNumberCard.setText(picture.getLike_number());
+    }
+
+    @Override
+    public int getItemCount() {
+        return pictures.size();
+    }
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView pictureCard;
+        private TextView usernameCard;
+        private TextView timeCard;
+        private TextView likeNumberCard;
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+
+            pictureCard     = (ImageView) itemView.findViewById(R.id.imageCard);
+            usernameCard    = (TextView) itemView.findViewById(R.id.userNameCard);
+            timeCard        = (TextView) itemView.findViewById(R.id.timeCard);
+            likeNumberCard  = (TextView) itemView.findViewById(R.id.likeNumberCard);
+        }
+    }
+
+}
--- app/src/main/java/com/platzi/platzigram/view/fragment/HomeFragment.java
+++ app/src/main/java/com/platzi/platzigram/view/fragment/HomeFragment.java
@@ -3,11 +3,19 @@ package com.platzi.platzigram.view.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.platzi.platzigram.R;
+import com.platzi.platzigram.adapter.PictureAdapterRecyclerView;
+import com.platzi.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,35 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        showToolBar("Home", false, view);
+        RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(buildPictures(), R.layout.cardview_picture, getActivity());
+
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("https://goo.gl/images/S9BEp7http://www.novalandtours.com/images/guide/guilin.jpg","Elon Musk","4 días", "3"));
+        pictures.add(new Picture("https://i.pinimg.com/originals/7f/35/e1/7f35e1a655e222faec0726e135627048.jpg","Steve Jobs","3 días", "10"));
+        pictures.add(new Picture("https://i.pinimg.com/736x/fb/b4/76/fbb476ddcd47700de52afb86d3889afe--winter-landscape-landscapes.jpg","Bill Gates","2 días", "9"));
+        return  pictures;
+    }
+
+    public void showToolBar(String title, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
